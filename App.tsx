0072ad9232706ff4f871e6636c40f0c2e639b80e
Lucas Henrique Gonçalves da Silveira <lucas.hgs03@gmail.com>
2023-08-31 02:47:20 -0300

    Text Component: Linked Text Variants to Text Component

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ import {Text} from './src/components/Text/Text';
 function App(): JSX.Element {
   return (
     <SafeAreaView>
-      <Text>Teste</Text>
+      <Text preset="headingLarge">Teste</Text>
     </SafeAreaView>
   );
 }
