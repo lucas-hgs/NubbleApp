124b5f849e86be910755f1cf744dc1f6edc9a923
Lucas Henrique Gonçalves da Silveira <lucas.hgs03@gmail.com>
2024-01-03 01:41:04 -0300

    Feat: useSignIn added to LoginScreen

--- src/domain/Auth/index.tsx
+++ src/domain/Auth/index.tsx
@@ -1,1 +1,8 @@
 export * from './hooks/useUser';
+export * from './hooks/useAuthSignIn';
+export * from './hooks/useAuthSignOut';
+
+export * from './authAdapter';
+export * from './authApi';
+export * from './authService';
+export * from './authTypes';
