4271e0d7ef8ec64433605dd28da67837dd22f74a
Lucas Henrique Gonçalves da Silveira <lucas.hgs03@gmail.com>
2024-01-03 16:19:02 -0300

    refactor: Preparing app for Authentication

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -2,8 +2,4 @@ import axios from 'axios';
 
 export const api = axios.create({
   baseURL: 'http://10.0.2.2:3333/',
-  headers: {
-    Authorization:
-      'Bearer NA.WnipY6hkfN7l-pYFoXneljb73TcDwc-_5Pj7oI0iDvF_VyRtQSkgaljIM3d6',
-  },
 });
