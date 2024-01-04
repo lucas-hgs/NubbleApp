043b6adb469b9711b9852eab0e39c719bd8f25f4
Lucas Henrique Gonçalves da Silveira <lucas.hgs03@gmail.com>
2024-01-03 23:29:46 -0300

    refactor: global state context (auth)

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import {AuthCredentialsProvider} from '@services';
 import {ThemeProvider} from '@shopify/restyle/dist/context';
 import {QueryClientProvider, QueryClient} from '@tanstack/react-query';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
@@ -12,14 +13,16 @@ const queryClient = new QueryClient();
 
 function App(): JSX.Element {
   return (
-    <QueryClientProvider client={queryClient}>
-      <SafeAreaProvider>
-        <ThemeProvider theme={theme}>
-          <Router />
-          <Toast />
-        </ThemeProvider>
-      </SafeAreaProvider>
-    </QueryClientProvider>
+    <AuthCredentialsProvider>
+      <QueryClientProvider client={queryClient}>
+        <SafeAreaProvider>
+          <ThemeProvider theme={theme}>
+            <Router />
+            <Toast />
+          </ThemeProvider>
+        </SafeAreaProvider>
+      </QueryClientProvider>
+    </AuthCredentialsProvider>
   );
 }
 
