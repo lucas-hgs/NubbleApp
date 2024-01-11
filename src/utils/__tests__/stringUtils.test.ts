d497630de95603e1f35722cc20893cd4ac972029
Lucas Henrique Gonçalves da Silveira <lucas.hgs03@gmail.com>
2024-01-11 16:29:26 -0300

    feat: added describe to stringUtils test

--- src/utils/__tests__/stringUtils.test.ts
+++ src/utils/__tests__/stringUtils.test.ts
@@ -1,11 +1,18 @@
 import {stringUtils} from '@utils';
 
-test('captalizeFirstLetter', () => {
-  stringUtils.captalizeFirstLetter('Ana maria');
-  stringUtils.captalizeFirstLetter('ANA MARIA');
-  stringUtils.captalizeFirstLetter('MaRIA');
+describe('stringUtils', () => {
+  describe('capitalizeFirstLetter', () => {
+    it('should capitalize the first letter of each word', () => {
+      expect(stringUtils.captalizeFirstLetter('Ana maria')).toBe('Ana Maria');
+      expect(stringUtils.captalizeFirstLetter('maria')).toBe('Maria');
+      expect(stringUtils.captalizeFirstLetter('ANA maria')).toBe('Ana Maria');
+      expect(stringUtils.captalizeFirstLetter('anA MARIA')).toBe('Ana Maria');
+      expect(stringUtils.captalizeFirstLetter('anA mARIA')).toBe('Ana Maria');
+    });
 
-  const nome = stringUtils.captalizeFirstLetter('Ana maria');
-
-  expect(nome).toBe('Ana Maria');
+    it('should be leading/trailing spaces', () => {
+      expect(stringUtils.captalizeFirstLetter(' Ana Maria')).toBe('Ana Maria');
+      expect(stringUtils.captalizeFirstLetter('Ana Maria ')).toBe('Ana Maria');
+    });
+  });
 });
