f70c6fcc33b5883022de03e3f94aec869125cf22
Lucas Henrique Gonçalves da Silveira <lucas.hgs03@gmail.com>
2024-01-12 20:43:33 -0300

    cleaning tests

--- src/components/Button/__tests__/Button.test.tsx
+++ src/components/Button/__tests__/Button.test.tsx
@@ -1,21 +1,40 @@
 import React from 'react';
 
-import {fireEvent, render} from 'test-utils';
+import {fireEvent, render, screen} from 'test-utils';
 
-import {Button} from '../Button';
+import {Button, ButtonProps} from '../Button';
+
+function renderComponent(props: Partial<ButtonProps>) {
+  render(<Button title="button title" {...props} />);
+
+  const titleElement = screen.getByText('button title', {exact: false});
+
+  return {
+    titleElement,
+  };
+}
 
 describe('<Button />', () => {
   it('calls the onPress function when the user press the button', () => {
     const mockedOnPress = jest.fn();
 
-    const {getByText} = render(
-      <Button title="button title" onPress={mockedOnPress} />,
-    );
-
-    const titleElement = getByText('button title', {exact: false});
+    const {titleElement} = renderComponent({onPress: mockedOnPress});
 
     fireEvent.press(titleElement);
 
     expect(mockedOnPress).toHaveBeenCalled();
+  });
+
+  it('does not call onPress function when it is disabled and it is pressed', () => {
+    const mockedOnPress = jest.fn();
+
+    const {titleElement} = renderComponent({
+      onPress: mockedOnPress,
+      disabled: true,
+    });
+
+    fireEvent.press(titleElement);
+
+    expect(mockedOnPress).not.toHaveBeenCalled();
   });
 });
