2d318348b1972a8bc51251de5189f1ec5be8c7c0
Lucas Henrique Gonçalves da Silveira <lucas.hgs03@gmail.com>
2024-01-15 00:12:01 -0300

    tested all the PostBottom component

--- src/components/PostItem/components/__tests__/PostBottom.test.tsx
+++ src/components/PostItem/components/__tests__/PostBottom.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import {Post} from '@domain';
-import {render, screen} from 'test-utils';
+import {fireEvent, render, screen} from 'test-utils';
 
 import {PostBottom} from '../PostBottom';
 
@@ -20,12 +20,50 @@ const mockedPost: Post = {
   },
 };
 
+const mockedNavigate = jest.fn();
+
+jest.mock('@react-navigation/native', () => {
+  const originalModule = jest.requireActual('@react-navigation/native');
+
+  return {
+    ...originalModule,
+    useNavigation: () => ({
+      navigate: mockedNavigate,
+    }),
+  };
+});
+
 describe('<PostBottom />', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('does not show the comment link if it has no comment', () => {
     render(<PostBottom {...mockedPost} commentCount={0} />);
 
     const commentLinkElement = screen.queryByText(/comentário/);
 
     expect(commentLinkElement).toBeFalsy();
+  });
+
+  it('navigates to PostCommentScreen when pressing the comment link', () => {
+    render(<PostBottom {...mockedPost} commentCount={4} />);
+
+    const commentLinkElement = screen.getByText(/comentário/);
+
+    fireEvent.press(commentLinkElement);
+
+    expect(mockedNavigate).toHaveBeenCalledWith('PostCommentScreen', {
+      postId: mockedPost.id,
+      postAuthorId: mockedPost.author.id,
+    });
+  });
+
+  it('should show ver comentário when you have only 1 comment', () => {
+    render(<PostBottom {...mockedPost} commentCount={1} />);
+
+    const commentLinkElement = screen.getByText(/comentário/);
+
+    expect(commentLinkElement.props.children).toEqual('ver comentário');
   });
 });
