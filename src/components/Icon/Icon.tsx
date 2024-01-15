2aeec1fab35758ff94cfb24a76889c90d65ac2df
Lucas Henrique Gonçalves da Silveira <lucas.hgs03@gmail.com>
2024-01-14 21:09:01 -0300

    test password props icon

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -50,7 +50,7 @@ export function Icon({
 
   if (onPress) {
     return (
-      <Pressable hitSlop={10} onPress={onPress}>
+      <Pressable testID={name} hitSlop={10} onPress={onPress}>
         <SVGIcon size={size} color={colors[color]} />
       </Pressable>
     );
