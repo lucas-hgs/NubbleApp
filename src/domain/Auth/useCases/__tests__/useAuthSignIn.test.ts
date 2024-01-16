65bd8e33129e08738524caab25acce0e080eb9cc
Lucas Henrique Gonçalves da Silveira <lucas.hgs03@gmail.com>
2024-01-16 00:45:21 -0300

    testing useAuthSignIn hook

--- src/domain/Auth/useCases/__tests__/useAuthSignIn.test.ts
+++ src/domain/Auth/useCases/__tests__/useAuthSignIn.test.ts
@@ -1,15 +1,38 @@
 import {renderHook} from '@testing-library/react-native';
-import {AllTheProviders} from 'test-utils';
+import {AllTheProviders, waitFor} from 'test-utils';
 
+import {authService} from '../../authService';
 import {useAuthSignIn} from '../useAuthSignIn';
 
+import {mockedAuthCredentials} from './mockedData/mocks';
+
+const mockedSaveCredentials = jest.fn();
+
+jest.mock('@services', () => {
+  const originalModule = jest.requireActual('@services');
+  return {
+    ...originalModule,
+    useAuthCredentials: () => ({
+      saveCredentials: mockedSaveCredentials,
+    }),
+  };
+});
+
 describe('useAuthSignIn', () => {
-  it('saves credentials if the sign-in successfully', () => {
+  it('saves credentials if the sign-in successfully', async () => {
+    jest
+      .spyOn(authService, 'signIn')
+      .mockResolvedValueOnce(mockedAuthCredentials);
+
     const {result} = renderHook(() => useAuthSignIn(), {
       wrapper: AllTheProviders,
     });
 
-    console.log(result);
+    result.current.signIn({email: 'lucas@gmail.com', password: '123456'});
+
+    await waitFor(() => expect(result.current.isSuccess).toBe(true));
+
+    expect(mockedSaveCredentials).toHaveBeenCalledWith(mockedAuthCredentials);
   });
 
   it('calls the onError function with a message if sign-in fails', () => {
