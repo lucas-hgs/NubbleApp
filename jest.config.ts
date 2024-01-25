1953c9624fb3f6639d871acf72faf2ffc5ea30ed
Lucas Henrique Gonçalves da Silveira <lucas.hgs03@gmail.com>
2024-01-25 17:58:24 -0300

    added ts support to jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,18 @@
-module.exports = {
+import type {Config} from 'jest';
+
+const config: Config = {
   preset: 'react-native',
-  collectCoverageFrom: [
-    'src/{components,utils,hooks,domain}/**/*.{js,jsx,ts,tsx}',
-  ],
-  coveragePathIgnorePatterns: ['/node_modules', 'index'],
+  setupFiles: ['<rootDir>/src/test/jestSetup.ts'],
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   moduleDirectories: ['node_modules', './src/test'],
   modulePathIgnorePatterns: ['.*/mockedData/.*'],
-  setupFiles: ['<rootDir>/src/test/jestSetup.ts'],
+  collectCoverageFrom: [
+    'src/{components,utils,hooks,domain}/**/*.{js,jsx,ts,tsx}',
+  ],
+  coveragePathIgnorePatterns: ['/node_modules/', 'index'],
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?|react-native-safe-area-context)/)',
   ],
 };
+
+export default config;
