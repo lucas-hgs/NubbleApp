0dbb35ba2f4b3435b17e0e4bb443076163dccb74
Lucas Henrique Gonçalves da Silveira <lucas.hgs03@gmail.com>
2024-02-03 00:32:59 -0300

    Teste: Configuração React Navigation

--- src/routes/AppStack.tsx
+++ src/routes/AppStack.tsx
@@ -10,6 +10,7 @@ import {AppTabBottomTabParamList, AppTabNavigator} from './AppTabNavigator';
 export type AppStackParamList = {
   AppTabNavigator: NavigatorScreenParams<AppTabBottomTabParamList>;
   SettingsScreen: undefined;
+  SearchScreen: undefined;
   PostCommentScreen: {
     postId: number;
     postAuthorId: number;
@@ -21,14 +22,18 @@ export type AppStackParamList = {
 
 const Stack = createNativeStackNavigator<AppStackParamList>();
 
-export function AppStack() {
+interface Props {
+  initialRouteName?: keyof AppStackParamList;
+}
+
+export function AppStack({initialRouteName = 'AppTabNavigator'}: Props) {
   return (
     <Stack.Navigator
       screenOptions={{
         headerShown: false,
         fullScreenGestureEnabled: true,
       }}
-      initialRouteName="AppTabNavigator">
+      initialRouteName={initialRouteName}>
       <Stack.Screen name="AppTabNavigator" component={AppTabNavigator} />
       <Stack.Screen name="SettingsScreen" component={SettingsScreen} />
       <Stack.Screen name="PostCommentScreen" component={PostCommentScreen} />
